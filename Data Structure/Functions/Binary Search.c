a077f500038722b5766a2b871578b5d00d840d89
YodaDevs <mateusmonteiroasa@gmail.com>
2018-10-17 11:44:25 -0300

    Add sorting functions

---
+++ Data Structure/Functions/Binary Search.c
@@ -0,0 +1,18 @@
+int binary_search(int *v, int size, int element){
+    
+    int begin = 0;
+    int end = size - 1;
+    int middle
+    ;
+    while (begin <= end) {
+            middle = (begin + end) / 2;
+        if (v[middle] < element) {
+            begin = middle + 1;
+        } else if (v[middle] > element) {
+            end = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+    return -1;
+}
