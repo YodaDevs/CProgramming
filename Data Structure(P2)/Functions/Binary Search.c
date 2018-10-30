586d79d8ca32a082147926a3088118fd8d5969d2
Mateus Monteiro <mateusmonteiroasa@gmail.com>
2018-10-29 21:29:22 -0300

    Update Binary Search.c

--- Data Structure(P2)/Functions/Binary Search.c
+++ Data Structure(P2)/Functions/Binary Search.c
@@ -2,8 +2,7 @@ int binary_search(int *v, int size, int element){
     
     int begin = 0;
     int end = size - 1;
-    int middle
-    ;
+    int middle;
     while (begin <= end) {
             middle = (begin + end) / 2;
         if (v[middle] < element) {
