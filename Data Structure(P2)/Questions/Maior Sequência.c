0b0e9b87a447ab90cb52016b8b404d433270e92d
YodaDevs <mateusmonteiroasa@gmail.com>
2019-06-28 19:16:31 -0300

    Adicionando Maior Sequência corrigida

--- Data Structure(P2)/Questions/Maior Sequência.c
+++ Data Structure(P2)/Questions/Maior Sequência.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -24,30 +25,33 @@ struct node* add (struct node *head, int item,int index){
   new_node->item = item;
   new_node->index = index;
   new_node->next = head;
+  new_node->sequence = 0;
   return new_node;
 }
 
 void print_linked_list (struct node *head){
   if (!is_empty(head)) {
-    printf("[%d]", head -> item );
+    printf("item: [%d] ", head -> item );
+    printf("index: [%d] ", head -> index );
+    printf("sequence: [%d]\n", head -> sequence);
     print_linked_list(head -> next);
   }
 }
 
-int FuncaoSequencia(struct node *head,int item){
-    if(item == 1) return 0;
-    head->sequence = 1 + FuncaoSequencia(head->next,head->next->item);
+int FuncaoSequencia(struct node *head){
+    if(is_empty(head) || head->item == 49) return 0;
+    return 1 + FuncaoSequencia(head->next);
 }
 
-void navegacao(int (*f)(struct node *head, int item), struct node *head){
+void navegacao(struct node *head){
   if (!is_empty(head)) {
-    head->sequence = (*f)(head,head->item);
-    navegacao((*f)(head,head->next->item), head -> next);
+    head->sequence = FuncaoSequencia(head);
+    navegacao(head->next);
   }
 }
 
 void AchandoMaiorSequencia(int A[], struct node *head){
-    if(!is_empty(head)) return ;
+    if(is_empty(head)) return ;
     else if(A[1] < head->sequence){
         A[0] = head->index;
         A[1] = head->sequence;
@@ -56,17 +60,50 @@ void AchandoMaiorSequencia(int A[], struct node *head){
 }
 
 int main() {
-    struct node* list = create_linked_list();
-    int a,i=0, A[2];
-    A[0] = -1;
-    A[1] = -1;
-    while(scanf("%d", &a)!=EOF){
-    getchar();
-    list = add(list,a,i);
-    i++;
+    int k=0,item,i=0, A[2];
+    
+    char c, *str = NULL;
+    int l = 0, j = 1;
+    str = (char *) malloc(10 * sizeof(char) + 1);
+    while ((c = getchar()) != EOF)
+    {
+      if (j == 10)
+      {
+        str = (char *) realloc(str, strlen(str) + 10 * sizeof(char) + 1);
+        j = 1;
+      }
+      *(str + l * sizeof(char)) = c;
+      l++;
+      j++;
     }
-    navegacao(FuncaoSequencia,list);
-    AchandoMaiorSequencia(A,list);
-    printf("%d %d\n", (A[0]-A[1]+1), A[0]);
+    *(str + l * sizeof(char)) = '\0';
+    
+    i=0;
+    j=0;
+    while(1){
+        if(j >= strlen(str)-1) break;
+        //criando a lista encadeado para a poss�vel opera��o
+        struct node* list = create_linked_list();
+        
+        for( ; str[j]!='\n'; j++){
+            //adicionando itens a lista encadeada
+              list = add(list,str[j],i);
+              i++;
+        }
+        
+        if(str[j] == '\n') j++;
+        
+        navegacao(list);
+        
+        A[0] = -1;
+        A[1] = -1;
+        
+        AchandoMaiorSequencia(A,list);
+        printf("%d %d\n", (A[0]-A[1]+1), A[0]); // A[0] encontra-se o indice de maior sequencia, A[1] encontrase a sequencia dele
+        //resetando i para 0 e free para lista encadeada
+        i=0;
+        free(list);
+    }
+    free(str);
 	return 0;
 }
