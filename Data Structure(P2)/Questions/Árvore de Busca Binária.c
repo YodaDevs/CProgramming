c4dce4f3ce7810cf779173bd7f59d3b100502da8
YodaDevs <mateusmonteiroasa@gmail.com>
2019-08-02 12:40:01 -0300

    Update Árvore de Busca Binária

--- Data Structure(P2)/Questions/Árvore de Busca Binária.c
+++ Data Structure(P2)/Questions/Árvore de Busca Binária.c
@@ -3,15 +3,69 @@
 #include <math.h>
 #include <string.h>
 
+
+// BEGIN STRUCT BINARY TREE
 typedef struct binary_tree {
     int item;
+    int count;
     struct binary_tree *left;
     struct binary_tree *right;
 }binary_tree;
+// END OF STRUCT BINARY TREE
 
-int array_number[100];
-int count = 0;
-int id = 0;
+#define MAX_STACK_SIZE 100
+
+// BEGIN STACK STRUCT WITH ARRAY
+typedef struct stack {
+    int current_size; 
+    char items[MAX_STACK_SIZE];
+}stack;
+// END OF STACK STRUCT
+
+// STACK FUNCTIONS
+stack* create_stack() {
+
+    stack *new_stack = (stack*) malloc(sizeof(stack));
+    new_stack->current_size = 0;
+    return new_stack;
+}
+
+int is_stack_empty(stack *stack){
+    return (stack->current_size == 0);
+}
+
+void push(stack *stack, int item) {
+
+    if (stack->current_size == MAX_STACK_SIZE) {
+        //printf("Stack overflow");
+    } else {
+        stack->items[stack->current_size++] = item;
+    }
+
+}
+
+int pop(stack *stack) {
+    
+    if (is_stack_empty(stack)) {
+        //printf("Stack underflow");
+        return -1;
+
+    } else {
+        return stack->items[--stack->current_size];
+    }
+}
+int peek(stack *stack) {
+    
+    if (is_stack_empty(stack)) {
+        //printf("Stack underflow");
+        return -1;
+
+    } else {
+        return stack->items[stack->current_size - 1];
+    }
+}
+//END OF STACK FUNCTIONS
+
 
 binary_tree* create_empty_binary_tree(){
     return NULL;
@@ -24,56 +78,125 @@ int is_empty(binary_tree *bt){
 binary_tree* create_binary_tree(int item, binary_tree *left, binary_tree *right){
     binary_tree *new_binary_tree = (binary_tree*) malloc(sizeof(binary_tree));
     new_binary_tree->item = item;
+    new_binary_tree->count = 0;
     new_binary_tree->left = left;
     new_binary_tree->right = right;
     return new_binary_tree;
 }
 
-binary_tree* add(binary_tree *bt, int item){
-    if (bt == NULL) {
-        bt = create_binary_tree(item,NULL, NULL);
-    } 
-    else if (bt->item > item) {
-        bt->left = add(bt->left, item);
-    } 
-    else {
-        bt->right = add(bt->right, item);
-    }
-    return bt;
-}
-
-int boolean(binary_tree* bt){
-    if(array_number[id] != bt->item){
-        id++;
-        return 1;
-    }
-    else {
-        id++;
-        return 0;
-    }
-}
-
-void relationship_array_with_tree(binary_tree *bt){
-    if (!is_empty(bt)) {
-        count = count + boolean(bt);
-        relationship_array_with_tree(bt->left);
-        relationship_array_with_tree(bt->right);
-    }
-}
-
 void print_pre_order(binary_tree *bt){
     if (!is_empty(bt)) {
-        printf("[%d] ", bt->item);
-       // printf(" left-> ");
+        printf("[%d]", bt->item);
         print_pre_order(bt->left);
-       // printf(" Right-> ");
         print_pre_order(bt->right);
     }
 }
 
+int Find_Index(char str[], int begin, int end){
+    stack* aux_stack = create_stack();
+
+    /*while(str[begin] >= 48 && str[begin]<=57 ){
+        begin++;
+    }*/
+
+    if(str[begin] == '('){
+        push(aux_stack,str[begin]);
+    }
+
+    while(!is_stack_empty(aux_stack)){
+        begin++;
+        if(str[begin] == '('){
+            push(aux_stack,str[begin]);
+        }
+        if(str[begin] == ')'){
+            pop(aux_stack);
+        }
+    }
+
+    return begin;
+
+}
+
+binary_tree* build_BinaryTree_from_string(char str[],int begin,int end){
+    int number = -1, index_left_end = 0, index_right_begin = 0;
+
+    if(begin >= end) return NULL;
+
+    if(str[begin] >= 48 && str[begin]<=57){
+        number = (str[begin] - 48);
+        begin++;
+        while(str[begin] >= 48 && str[begin]<=57 ){
+            number = number*10;
+            number = number + (str[begin] - 48);
+            begin++;
+        }
+    }
+    if(number == -1) return NULL;
+    binary_tree* root = create_binary_tree(number,NULL,NULL);
+
+    index_left_end = Find_Index(str,begin,end);
+
+    root->left = build_BinaryTree_from_string(str,begin + 1,index_left_end - 1);
+    index_right_begin = Find_Index(str,begin,end) + 1;
+    if(str[index_right_begin] == ' ') index_right_begin++;
+    root->right = build_BinaryTree_from_string(str,index_right_begin + 1,end - 1);
+    
+    return root;
+}
+
+
+void is_binary_tree(binary_tree* root){
+    if(!is_empty(root->left)){
+        if(root->item > root->left->item) ;
+        else root->count++; 
+    }
+    if(!is_empty(root->right)){
+        if(root->item < root->right->item) ;
+        else root->count++;
+    }
+    return;
+}
+
+void navegation(binary_tree *bt){
+    if (!is_empty(bt)) {
+        is_binary_tree(bt);
+        navegation(bt->left);
+        navegation(bt->right);
+    }
+}
+
+int find_count(binary_tree *bt){
+    if (!is_empty(bt)) {
+        if(bt->count > 0) return 1;
+        return 0 + find_count(bt->left) + find_count(bt->right);
+    }
+}
+
+// Function to construct brackets with binary tree 
+void print_tree_brackets(binary_tree* root) 
+{ 
+    // bases case 
+    if (root == NULL){ 
+        printf("("); 
+        printf(")");
+        return; 
+    }
+    if(root != NULL){
+        printf("(%d", root->item);
+    }
+    // for left subtree 
+    //printf("");
+    print_tree_brackets(root->left);
+    // for right subtree
+    //printf("  ");
+    print_tree_brackets(root->right); 
+
+    printf(")");
+} 
+
 int main()
 {
-    int i,l=0, j=1,number;
+    int i,l=0, j=1,number, bol;
     binary_tree* bt = create_empty_binary_tree();
     
     char c, *str = NULL;
@@ -89,35 +212,24 @@ int main()
       j++;
     }
     *(str + l * sizeof(char)) = '\0';
+    /*for (i = 0; i < strlen(str); i++){
+        printf("%c", str[i]);
+    }
+    printf("\n");*/
     
-    for(i = 0, j = 0; i < strlen(str) ; i++){
-        number=0;
-        if(str[i] >= 48 && str[i]<=57){
-            number = str[i] - 48;
-            i++;
-            while(str[i] >= 48 && str[i]<=57 ){
-                number = number*10;
-                number = number + (str[i] - 48);
-                i++;
-            }
-            bt = add(bt,number);
-            array_number[j] = number;
-            j++;
-        }
-    }
-    free(str);
-    for(i = 0; i < j; i++){
-        printf("[%d] ", array_number[i]);
-    }
-    printf("\n");
-    print_pre_order(bt);
-    printf("\n");
-    relationship_array_with_tree(bt);
-    if(count){
+    bt = build_BinaryTree_from_string(str,1,strlen(str)-1);
+    
+    //print_tree_brackets(bt);
+    //printf("\n");
+    navegation(bt);
+    bol = find_count(bt);
+    //printf("%d\n", bol);
+    if(bol && !is_empty(bt)){
         printf("FALSO\n");
     }
     else{
         printf("VERDADEIRO\n");
     }
+    
     return 0;
 }
